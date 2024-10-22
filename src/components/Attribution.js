2dd48a8f42bd371a397b37a77af140a44219e4c6
vinodpanzade <vinodpanzade64@gmail.com>
2024-10-22 15:44:31 +0530

    change

---
+++ src/components/Attribution.js
@@ -0,0 +1,7 @@
+export default function Attribution() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="https://travolgi.it" target="_blank" rel="noreferrer">Vinod</a></p>
+      </footer>
+   );
+}
